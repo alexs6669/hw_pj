0bcafba2b8ac498d645c67af024a5e457a0fde67
alexs6669 <schegoleff@gmail.com>
2021-05-24 17:49:41 +0300

    2. Подключил дефолтные пагинатор и фильтр 3. Запилил CRUD для трех моделей 4. Прописал урлы 5. Переопределил метод delete для заметок

--- TODO_list/pjapp/views.py
+++ TODO_list/pjapp/views.py
@@ -1,13 +1,78 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.generics import *
+from rest_framework.renderers import JSONRenderer
+from rest_framework.response import Response
+
 from pjapp.models import Project, TODO
-from .serializers import ProjectModelSerializer, TODOModelSerializer
+from .serializers import *
 
 
-class ProjectModelViewSet(ModelViewSet):
+class ProjectListView(ListAPIView):
+    # renderer_classes = [JSONRenderer]
     queryset = Project.objects.all()
     serializer_class = ProjectModelSerializer
 
 
-class TODOModelViewSet(ModelViewSet):
+class ProjectCreateView(CreateAPIView):
+    # renderer_classes = [JSONRenderer]
+    queryset = Project.objects.all()
+    serializer_class = ProjectModelSerializer
+
+
+class ProjectUpdateView(UpdateAPIView):
+    # renderer_classes = [JSONRenderer]
+    queryset = Project.objects.all()
+    serializer_class = ProjectModelSerializer
+
+
+class ProjectDetailView(RetrieveAPIView):
+    # renderer_classes = [JSONRenderer]
+    queryset = Project.objects.all()
+    serializer_class = ProjectModelSerializer
+
+
+class ProjectDeleteView(DestroyAPIView):
+    # renderer_classes = [JSONRenderer]
+    queryset = Project.objects.all()
+    serializer_class = ProjectModelSerializer
+
+
+class NoteListView(ListAPIView):
+    # renderer_classes = [JSONRenderer]
     queryset = TODO.objects.all()
     serializer_class = TODOModelSerializer
+
+
+class NoteCreateView(CreateAPIView):
+    # renderer_classes = [JSONRenderer]
+    queryset = TODO.objects.all()
+    serializer_class = TODOModelSerializer
+
+
+class NoteUpdateView(UpdateAPIView):
+    # renderer_classes = [JSONRenderer]
+    queryset = TODO.objects.all()
+    serializer_class = TODOModelSerializer
+
+
+class NoteDetailView(RetrieveAPIView):
+    # renderer_classes = [JSONRenderer]
+    queryset = TODO.objects.all()
+    serializer_class = TODOModelSerializer
+
+
+class NoteDeleteView(DestroyAPIView):
+    # renderer_classes = [JSONRenderer]
+    queryset = TODO.objects.all()
+    serializer_class = TODOModelSerializer
+
+    def delete(self, request, *args, **kwargs):
+        note = self.get_object()
+        serializer = TODOModelSerializer(note)
+        if note.is_active:
+            note.is_active = False
+        else:
+            note.is_active = True
+        note.save()
+
+        return Response(serializer.data)
+
