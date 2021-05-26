3ff54b7209b40643122d016723882d08991543df
alexs6669 <schegoleff@gmail.com>
2021-05-26 20:56:23 +0300

    изменил модели и сериализаторы для корректного отображения

--- TODO_list/pjapp/models.py
+++ TODO_list/pjapp/models.py
@@ -7,11 +7,18 @@ class Project(models.Model):
     repo_link = models.URLField(verbose_name='Ссылка на репозиторий', blank=True)
     users = models.ManyToManyField(User, verbose_name='Пользователи')
 
+    def __str__(self):
+        return self.name
+
 
 class TODO(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE, verbose_name='Название проекта')
     user = models.ForeignKey(User, models.CASCADE, verbose_name='Пользователь')
+    title = models.CharField(verbose_name='Заголовок', max_length=64, default=f'Заметка')
     text = models.TextField(verbose_name='Текст заметки', max_length=512, blank=True)
     created = models.DateTimeField(verbose_name='Создана', auto_now_add=True)
     updated = models.DateTimeField(verbose_name='Изменена', auto_now=True)
     is_active = models.BooleanField(verbose_name='Активна', default=True)
+
+    def __str__(self):
+        return f'{self.project} {self.title} {self.user} {self.is_active}'
--- TODO_list/pjapp/serializers.py
+++ TODO_list/pjapp/serializers.py
@@ -1,14 +1,20 @@
-from rest_framework.serializers import HyperlinkedModelSerializer, ModelSerializer
+from rest_framework.serializers import HyperlinkedModelSerializer, ModelSerializer, SlugRelatedField
 from pjapp.models import Project, TODO
+from authapp.models import User
 
 
 class ProjectModelSerializer(ModelSerializer):
+    users = SlugRelatedField(many=True, read_only=True, slug_field='username')
+
     class Meta:
         model = Project
-        fields = '__all__'
+        fields = ('name', 'repo_link', 'users',)
 
 
 class TODOModelSerializer(ModelSerializer):
+    user = SlugRelatedField(read_only=True, slug_field='username')
+    project = SlugRelatedField(read_only=True, slug_field='name')
+
     class Meta:
         model = TODO
-        fields = '__all__'
+        fields = ('project', 'title', 'text', 'user', 'created', 'updated', 'is_active',)
