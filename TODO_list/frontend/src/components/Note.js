c1f5b33c8ede5a43f350314eb44eede68c1a0cb1
alexs6669 <schegoleff@gmail.com>
2021-05-27 13:01:33 +0300

    добавил немного красоты=)

--- TODO_list/frontend/src/components/Note.js
+++ TODO_list/frontend/src/components/Note.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import ActiveImage from './images/true.png'
+import NotActiveImage from './images/false.png'
 
 const NoteItem = ({note}) => {
     return (
         <tr className='info'>
-            <td>{note.is_active ? 'Активна' : 'Не активна'}</td>
+            <td>{note.is_active ? <img src={ActiveImage} width={40}
+                                       height={40}/> : <img src={NotActiveImage} width={40} height={40}/>}</td>
             <td>{note.project}</td>
             <td>{note.title}</td>
             <td>{note.user}</td>
