db3aff9be7fbabfcf2f4893c0dc3407fd6683ff0
alexs6669 <schegoleff@gmail.com>
2021-05-30 14:32:35 +0300

    забыл зарегать модели проекта и заметки в админке. Исправился

--- TODO_list/authapp/admin.py
+++ TODO_list/authapp/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from authapp.models import User
+from pjapp.models import Project, TODO
 
 admin.site.register(User)
+admin.site.register(Project)
+admin.site.register(TODO)
